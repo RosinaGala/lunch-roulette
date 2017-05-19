e3f698b79da171f2e408264efb6d4bd3c31306e0
Sina Burow <sina.burow@sinnerschrader.com>
2017-05-19 16:57:17 +0200

    more react-ish

--- src/atoms/Answer.js
+++ src/atoms/Answer.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
 class Answer extends Component {
+    componentDidMount() { 
+      const spazieren = ["Oh ja, ausgiebig.", "LOL nein.", "Eine kleine Runde.", "Mit Sicherheit nicht."]; 
+      const restaurants = ["Coco Thai",  "Es ist Dönerstag", "Vapiano lol", "Tawarayayay", "Ede Vesperbot", "Bonobo", "BE WE IH TE", "Auf zum Inder!", "Stones", "Soul Kitchen"];
+      this.changeBackground(); 
+      this.printAnswer(".input", restaurants); 
+      this.printAnswer(".walk", spazieren); 
+  } 
+
+  //generates random number
+  randomNumber =  (min, max) => Math.floor(Math.random() * (max - min));
+
+  // gets target element and fills in random winner output depending on the chosen array
+  printAnswer = (target, array) => document.querySelector(target).innerHTML = array[this.randomNumber(0, array.length)];
   
+  changeBackground = () => {
+    let body = document.getElementsByTagName("body")[0];
+    body.className += "result";
+  };
+
   render() {
     return (
         <section id="answer">
--- src/atoms/Formular.js
+++ src/atoms/Formular.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
 
 class Formular extends Component {
-  //generates random number
-  winner =  (min, max) => Math.floor(Math.random() * (max - min));
-  // gets target element and fills in random winner output depending on the chosen array
-  printAnswer = (target, array) => document.querySelector(target).innerHTML = array[this.winner(0, array.length)];
-  removeStuff = (which, how) => document.getElementById(which).style.display = how;
-  NeedRemoved = () => {
-    var body = document.getElementsByTagName("body")[0];
-    this.removeStuff("remove", "none");
-    this.removeStuff("answer", "flex");
-    body.className += "result";
-  };
-  click = () => {
-    var spazieren = ["Oh ja, ausgiebig.", "LOL da fuq?", "Eine kleine Runde.", "Mit Sicherheit nicht."];
-    var restaurants = ["Coco Thai",  "Es ist Dönerstag", "Vapiano lol", "Tawarayayay", "Ede Vesperbot", "Bonobo", "BE WE IH TE", "Auf zum Inder!", "Stones", "Soul Kitchen"];
-    this.NeedRemoved();
-    this.printAnswer(".input", restaurants);
-    this.printAnswer(".walk", spazieren);
-  };
+  constructor(props){ 
+    super(props); 
+    this.handleClick= this.handleClick.bind(this); 
+  }
+
+  handleClick(e) { 
+    this.props.handler(); 
+  }; 
+  
   render() {
     return (
       <div id="remove">
         <section className="buttonWrap">
-          <button role="button" onClick={this.click.bind(this)}>Jetzt unbedingt<br />HIER KLICKEN</button>
+          <button role="button" type="button" onClick={this.handleClick}>Jetzt unbedingt<br />HIER KLICKEN</button>
         </section>
       </div>
     );
